0a5156ec3e795a71833e1296905e404285eba3ce
SHEA <nacho.dieguez18@gmail.com>
2023-04-05 13:15:57 -0300

    threejs scene implemented

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -15,6 +15,33 @@ const Customizer = () => {
 
 	const snap = useSnapshot(state);
 
+	const [file, setFile] = useState('');
+
+	const [propmt, setPropmt] = useState('');
+	const [generatingImg, setGeneratingImg] = useState(false);
+
+	const [activeEditorTab, setActiveEditorTab] = useState('');
+	const [activeFilterTab, setActiveFilterTab] = useState({
+		logoShirt: true,
+		stylishShirt: false,
+	});
+
+
+	//show tab content depending on the active tag
+	const generateTabContent = () => {
+		switch (activeEditorTab) {
+			case "colorpicker":
+				return <ColorPicker/>
+			case "filepicker":
+				return <FilePicker/>
+			case "aipicker":
+				return <AIPicker/>
+
+			default:
+				return null;
+		}
+	}
+
 	return (
 		<AnimatePresence>
 			{!snap.intro && (
@@ -30,9 +57,11 @@ const Customizer = () => {
 									<Tab
 										key={tab.name}
 										tab={tab}
-										handleClick={() => {}}
+										handleClick={() => setActiveEditorTab(tab.name)}
 									/>
 								))}
+
+								{generateTabContent()}
 							</div>
 						</div>
 					</motion.div>
